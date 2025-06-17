cbb5d0923c3e2ab3372e661bf17700ab664800eb
DoroStanislav <stanislav4zoom@gmail.com>
2025-06-17 19:38:43 +0300

    Implement complete Result entity and controller with full CRUD, validations, and foreign key checks

---
+++ F1StatsAPI/Controllers/ResultController.cs
@@ -0,0 +1,190 @@
+﻿using F1StatsAPI.Data;
+using F1StatsAPI.Migrations;
+using F1StatsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System.Data.Common;
+
+namespace F1StatsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ResultController : ControllerBase
+    {
+        private readonly F1StatsContext _context;
+
+        public ResultController(F1StatsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Result>>> GetResults()
+        {
+            return await GetFullResultQuery().ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Result>> GetResult(int id)
+        {
+            var existingResult = await GetFullResultQuery().FirstOrDefaultAsync(r => r.Id == id);
+
+            if (existingResult == null)
+            {
+                return NotFound();
+            }
+
+            return existingResult;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddResult(Result result)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var validationResult = await ValidateForeignKeys(result);
+            if (validationResult != null)
+                return validationResult;
+
+            await _context.Results.AddAsync(result);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbException)
+            {
+                return StatusCode(500, "A database error occurred while adding the Race.");
+            }
+
+            catch (Exception)
+            {
+                return StatusCode(500, "An unexpectex error occured.");
+            }
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateResult(int id, Result result)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != result.Id)
+            {
+                return BadRequest("ID mismatch");
+            }
+
+            var existingResult = await GetFullResultQuery().FirstOrDefaultAsync(r => r.Id == id);
+            if (existingResult == null)
+            {
+                return NotFound();
+            }
+
+            var validation = await ValidateForeignKeys(result);
+            if (validation != null)
+            {
+                return validation;
+            }
+
+            existingResult.GrandPrixId = result.GrandPrixId;
+            existingResult.TeamId = result.TeamId;
+            existingResult.DriverId = result.DriverId;
+            existingResult.CarId = result.CarId;
+
+            existingResult.Position = result.Position;
+            existingResult.Time = result.Time;
+            existingResult.Points = result.Points;
+            existingResult.DidNotFinish = result.DidNotFinish;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbUpdateException)
+            {
+                return StatusCode(500, "A database error occurred while updating the result.");
+            }
+
+            catch (Exception)
+            {
+                return StatusCode(500, "An unexpected error occured.");
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteResult(int id)
+        {
+            var existingResult = await _context.Results.FindAsync(id);
+
+            if (existingResult == null)
+            {
+                return NotFound();
+            }
+
+            _context.Results.Remove(existingResult);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbUpdateException)
+            {
+                return StatusCode(500, "A database error occurred while deleting the result.");
+            }
+
+            catch (Exception)
+            {
+                return StatusCode(500, "An unexpected error occured.");
+            }
+
+            return NoContent();
+        }
+
+        private IQueryable<Result> GetFullResultQuery()
+        {
+            return _context.Results
+                .Include(g => g.GrandPrix)
+                .Include(t => t.Team)
+                .Include(d => d.Driver)
+                .Include(c => c.Car);
+        }
+
+        private async Task<ActionResult?> ValidateForeignKeys(Result result)
+        {
+            if (!await _context.GrandPrix.AnyAsync(g => g.Id == result.GrandPrixId))
+            {
+                return BadRequest($"GrandPrix with ID {result.GrandPrixId} does not exist.");
+            }
+
+            if (!await _context.Teams.AnyAsync(t => t.Id == result.TeamId))
+            {
+                return BadRequest($"Team with ID {result.TeamId} does not exist.");
+            }
+
+            if (!await _context.Drivers.AnyAsync(d => d.Id == result.DriverId))
+            {
+                return BadRequest($"Driver with ID {result.DriverId} does not exist.");
+            }
+
+            if (!await _context.Cars.AnyAsync(c => c.Id == result.CarId))
+            {
+                return BadRequest($"Car with ID {result.CarId} does not exist.");
+            }
+
+            return null;
+        }
+    }
+}
---
+++ F1StatsAPI/Migrations/20250616171740_AddResultEntity.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace F1StatsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResultEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GrandPrixId = table.Column<int>(type: "int", nullable: false),
+                    DriverId = table.Column<int>(type: "int", nullable: false),
+                    TeamId = table.Column<int>(type: "int", nullable: false),
+                    CarId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Results_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Results_Drivers_DriverId",
+                        column: x => x.DriverId,
+                        principalTable: "Drivers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Results_GrandPrix_GrandPrixId",
+                        column: x => x.GrandPrixId,
+                        principalTable: "GrandPrix",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Results_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_CarId",
+                table: "Results",
+                column: "CarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_DriverId",
+                table: "Results",
+                column: "DriverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_GrandPrixId",
+                table: "Results",
+                column: "GrandPrixId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_TeamId",
+                table: "Results",
+                column: "TeamId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Results");
+        }
+    }
+}
