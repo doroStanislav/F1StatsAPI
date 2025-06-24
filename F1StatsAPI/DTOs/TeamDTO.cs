510e55de1322da63adf85845a828443b53c1a366
DoroStanislav <stanislav4zoom@gmail.com>
2025-06-24 20:48:12 +0300

    Add TeamDTO and AutoMapper configuration

---
+++ F1StatsAPI/DTOs/TeamDTO.cs
@@ -0,0 +1,14 @@
+﻿using F1StatsAPI.Models;
+
+namespace F1StatsAPI.DTOs
+{
+    public class TeamDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string TeamChief { get; set; } = string.Empty;
+        public int WorldChampionships { get; set; }
+        public int FoundationYear { get; set; }
+        public string? CarChassisCode { get; set; }
+        public List<string>? DriverNames { get; set; }
+    }
+}
--- F1StatsAPI/Services/ITeamService.cs
+++ F1StatsAPI/Services/ITeamService.cs
@@ -1,11 +1,12 @@
 ﻿using F1StatsAPI.Models;
+using F1StatsAPI.DTOs;
 
 namespace F1StatsAPI.Services
 {
     public interface ITeamService
     {
-        Task<IEnumerable<Team>> GetTeamsAsync();
-        Task<Team?> GetTeamByIdAsync(int id);
+        Task<IEnumerable<TeamDTO>> GetTeamsAsync();
+        Task<TeamDTO?> GetTeamByIdAsync(int id);
         Task<IEnumerable<Result>> GetTeamResultAsync(int id);
         Task<Team?> AddTeamAsync(Team team);
         Task<bool> UpdateTeamAsync(int id, Team team);
