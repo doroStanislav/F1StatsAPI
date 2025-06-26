a096052d546ffa59b271b0833ef57d06aaac9b44
DoroStanislav <stanislav4zoom@gmail.com>
2025-06-26 20:51:49 +0300

    Moved Result mapping to dedicated ResultProfile with custom resolver and FK projections

--- F1StatsAPI/Mappings/AutoMapperProfile.cs
+++ F1StatsAPI/Mappings/AutoMapperProfile.cs
@@ -13,22 +13,7 @@ namespace F1StatsAPI.Mappings
             
 
             
-            // Mapping fields from related GrandPrix entity (null-safe)
-            CreateMap<Result, ResultDTO>()
-                .ForMember(dest => dest.GrandPrixName,
-                    opt => opt.MapFrom(src => src.GrandPrix != null ? src.GrandPrix.Name : null)) //GrandPrix NAME
-
-                .ForMember(dest => dest.GrandPrixDate,
-                    opt => opt.MapFrom(src => src.GrandPrix != null ? src.GrandPrix.Date : (DateTime?)null)) //GrandPrix DATE
-
-                .ForMember(dest => dest.DriverName,
-                    opt => opt.MapFrom(src => src.Driver != null ? src.Driver.GivenName + " " + src.Driver.FamilyName : null)) //Driver FULL NAME
-
-                .ForMember(dest => dest.TeamName,
-                    opt => opt.MapFrom(src => src.Team != null ? src.Team.Name : null)) //Team NAME
-
-                .ForMember(dest => dest.RaceStatus,
-                    opt => opt.MapFrom<RaceStatusResolver>()); //Race STATUS
+            
         }
     }
 }
