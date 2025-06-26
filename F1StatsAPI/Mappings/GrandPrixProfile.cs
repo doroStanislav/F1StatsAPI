f68c377fa62fd942f00df1845558e3f00d379b78
DoroStanislav <stanislav4zoom@gmail.com>
2025-06-26 20:46:38 +0300

    Moved GrandPrix mapping to dedicated GrandPrixProfile

---
+++ F1StatsAPI/Mappings/GrandPrixProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using F1StatsAPI.Models;
+using F1StatsAPI.DTOs;
+
+namespace F1StatsAPI.Mappings
+{
+    public class GrandPrixProfile : Profile
+    {
+        public GrandPrixProfile() 
+        {
+            CreateMap<GrandPrix, GrandPrixDTO>();
+        }
+    }
+}
